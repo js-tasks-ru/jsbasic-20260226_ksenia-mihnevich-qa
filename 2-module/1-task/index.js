050be57c72ba02ebe114733de779b17aa75a6184
Ksenia Mihnevich <ksenia.mihnevich.qa@gmail.com>
2026-03-15 21:27:43 +0300

    Solution of Sum salary task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  let values = Object.values(salaries);
+  for (let value of values) {
+    if (Number.isFinite(value)) {
+      sum += value;
+    }
+  }
+  return sum;
 }
